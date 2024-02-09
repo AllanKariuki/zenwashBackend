45966abdf2b027792976fc79c189cc0e9ba65eb9
AllanKariuki <alankariuki24@gmail.com>
2024-02-09 07:27:37 +0300

    chore: app urls definition

--- zenwashBackend/urls.py
+++ zenwashBackend/urls.py
@@ -19,5 +19,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('vendors.urls'))
+    path('', include('users.urls')),
+    path('account/', include('account.urls')),
+    path('vendors/', include('vendors.urls')),
 ]
