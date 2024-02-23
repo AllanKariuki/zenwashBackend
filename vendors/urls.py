87bbea69030b703b62f6a4706023b4036b320fc9
AllanKariuki <alankariuki24@gmail.com>
2024-02-23 11:14:03 +0300

    chore: services and business types viewsets and models

--- vendors/urls.py
+++ vendors/urls.py
@@ -1,11 +1,19 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import VendorViewSet, ProductViewSet, OrderViewSet
+from .views import (
+    VendorViewSet,
+    ProductViewSet,
+    OrderViewSet,
+    ServicesViewSet,
+    BusinessTypeViewSet,
+)
 
 router = DefaultRouter()
 router.register('vendors', VendorViewSet, basename='vendor')
 router.register('products', ProductViewSet, basename='product')
 router.register('orders', OrderViewSet, basename='order')
+router.register('services', ServicesViewSet, basename='services')
+router.register('business_types', BusinessTypeViewSet, basename='business_types')
 
 urlpatterns = [
     path('', include(router.urls))
